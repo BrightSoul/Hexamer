b2671f1a128d902ff6f7f9e03be0ecb6b6c6b076
Moreno Gentili <brightsoul@tiscali.it>
2017-04-14 23:58:39 +0200

    Nested templates

--- src/Scripts/Login.ts
+++ src/Scripts/Login.ts
@@ -3,10 +3,15 @@
 class HelloViewModel {
     language: KnockoutObservable<string>
     framework: KnockoutObservable<string>
+    templateName: KnockoutObservable<string>
 
     constructor(language: string, framework: string) {
         this.language = ko.observable(language);
         this.framework = ko.observable(framework);
+        this.templateName = ko.observable("QuestionTypes/MultipleChoice");
+        setTimeout(() => {
+            this.templateName("QuestionTypes/Reorder");
+        }, 5000);
     }
 }
 export function init() {
--- src/wwwroot/js/app.js
+++ src/wwwroot/js/app.js
@@ -19,8 +19,13 @@ define("Login", ["require", "exports", "knockout"], function (require, exports, 
     exports.__esModule = true;
     var HelloViewModel = (function () {
         function HelloViewModel(language, framework) {
+            var _this = this;
             this.language = ko.observable(language);
             this.framework = ko.observable(framework);
+            this.templateName = ko.observable("QuestionTypes/MultipleChoice");
+            setTimeout(function () {
+                _this.templateName("QuestionTypes/Reorder");
+            }, 5000);
         }
         return HelloViewModel;
     }());
@@ -51,7 +56,18 @@ define("QuestionTypes/MultipleChoice", ["require", "exports", "knockout"], funct
     exports.__esModule = true;
     var HelloViewModel = (function () {
         function HelloViewModel(language, framework) {
-            alert(1);
+            this.language = ko.observable(language);
+            this.framework = ko.observable(framework);
+        }
+        return HelloViewModel;
+    }());
+    exports.HelloViewModel = HelloViewModel;
+});
+define("QuestionTypes/Reorder", ["require", "exports", "knockout"], function (require, exports, ko) {
+    "use strict";
+    exports.__esModule = true;
+    var HelloViewModel = (function () {
+        function HelloViewModel(language, framework) {
             this.language = ko.observable(language);
             this.framework = ko.observable(framework);
         }
